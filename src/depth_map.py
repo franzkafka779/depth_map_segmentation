4cd948dc31f4cd1f1fb1b0f188c562fb04433825
franzkafka779 <115647447+franzkafka779@users.noreply.github.com>
2024-06-05 12:06:56 +0900

    Create depth_map.py

---
+++ src/depth_map.py
@@ -0,0 +1,32 @@
+def generate_depth_map(image_left, image_right):
+    # 기존 코드에서 ORB 특징점 검출 및 매칭 코드 추가
+    orb = cv2.ORB_create()
+    keypoints_left, descriptors_left = orb.detectAndCompute(gray_left, None)
+    keypoints_right, descriptors_right = orb.detectAndCompute(gray_right, None)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(descriptors_left, descriptors_right)
+    matches = sorted(matches, key=lambda x: x.distance)
+    src_pts = np.float32([keypoints_left[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([keypoints_right[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    
+    # 깊이 맵 생성 부분 수정
+    disparity_map = np.zeros_like(gray_left)
+    for i in range(len(matches)):
+        disparity_map[int(src_pts[i][1]), int(src_pts[i][0])] = np.abs(src_pts[i][0] - dst_pts[i][0])
+    
+    return disparity_map
+
+# def generate_depth_map(image_left, image_right):
+#     # 스테레오 카메라로 촬영한 이미지를 입력받아 깊이 맵을 생성하는 함수
+
+#     # 스테레오 이미지를 그레이 스케일로 변환
+#     gray_left = cv2.cvtColor(image_left, cv2.COLOR_BGR2GRAY)
+#     gray_right = cv2.cvtColor(image_right, cv2.COLOR_BGR2GRAY)
+
+#     # 스테레오 매칭 알고리즘 설정
+#     stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+
+#     # 깊이 맵 생성
+#     depth_map = stereo.compute(gray_left, gray_right)
+
+#     return depth_map
